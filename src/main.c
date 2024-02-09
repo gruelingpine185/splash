a76f87d10dfbd0f8eb87495fa49bac7467fbc022
Adrian <66073333+gruelingpine185@users.noreply.github.com>
2024-02-08 19:51:37 -0500

    created window

--- src/main.c
+++ src/main.c
@@ -1,3 +1,17 @@
+#include "game.h"
+
+
 int main(void) {
+    game_t game = {0};
+    if(!create_game(&game)) return 1;
+
+    game.state = GAME_STATE_RUNNING;
+    while(game.state != GAME_STATE_QUIT) {
+        if(!render_game(&game)) return 1;
+
+        game_poll_events(&game);
+    }
+
+    destroy_game(&game);
     return 0;
 }
