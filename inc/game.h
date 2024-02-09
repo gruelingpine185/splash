592d264e6161a5675ba75db320aaa898d8ed28d2
Adrian <66073333+gruelingpine185@users.noreply.github.com>
2024-02-08 19:49:19 -0500

    added game state

--- inc/game.h
+++ inc/game.h
@@ -2,6 +2,12 @@
 #define GAME_H
 
 
+typedef enum {
+    GAME_STATE_QUIT,
+    GAME_STATE_RUNNING
+} game_state_t;
+
+
 typedef struct game_t game_t;
 
 typedef struct SDL_Window SDL_Window;
@@ -11,6 +17,7 @@ typedef struct SDL_Renderer SDL_Renderer;
 struct game_t {
     SDL_Window* window;
     SDL_Renderer* renderer;
+    game_state_t state;
 };
 
 
