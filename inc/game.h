8de87ffe21560dffbeb86b14c75ae82909bf785a
Adrian <66073333+gruelingpine185@users.noreply.github.com>
2024-02-10 03:18:52 -0500

    removed game.h

--- inc/game.h
+++ inc/game.h
@@ -2,34 +2,6 @@
 #define GAME_H
 
 
-typedef enum {
-    GAME_STATE_QUIT,
-    GAME_STATE_RUNNING
-} game_state_t;
-
-
-typedef struct game_t game_t;
-
-typedef struct SDL_Window SDL_Window;
-typedef struct SDL_Renderer SDL_Renderer;
-
-
-struct game_t {
-    SDL_Window* window;
-    SDL_Renderer* renderer;
-    game_state_t state;
-};
-
-
-#ifdef __cplusplus
-extern "C" {
-#endif // __cplusplus
-int create_game(game_t* _game);
-void destroy_game(game_t* _game);
-int render_game(game_t* _game);
-void game_poll_events(game_t* _game);
-#ifdef __cplusplus
 }
-#endif // __cplusplus
 
 #endif // GAME_H
