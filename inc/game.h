76fefd146933eed2214c78f3bda034f9d31886d0
Adrian <66073333+gruelingpine185@users.noreply.github.com>
2024-02-10 04:07:06 -0500

    added game class

--- inc/game.h
+++ inc/game.h
@@ -2,6 +2,31 @@
 #define GAME_H
 
 
+namespace splash {
+    class game;
+    class window;
+
+    enum class game_state;
+
+    
+    enum class game_state {
+        quit,
+        running
+    };
+
+
+    class game {
+    public:
+        game(const game& _game) = delete;
+        game();
+        ~game();
+
+        void poll_events(window& _win);
+        void render(window& _win);
+        game_state get_state() const;
+    private:
+        game_state _state;
+    };
 }
 
 #endif // GAME_H
